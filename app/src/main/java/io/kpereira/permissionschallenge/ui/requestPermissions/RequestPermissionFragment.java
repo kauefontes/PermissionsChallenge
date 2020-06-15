d53ea34b0457d2ef011a8bb8eb6eb616efc73f3a
Kaue Pereira <kpereira@motorola.com>
2020-06-15 03:26:27 -0400

    Now all the data showed on the UI is coming from the Room Database

--- app/src/main/java/io/kpereira/permissionschallenge/ui/requestPermissions/RequestPermissionFragment.java
+++ app/src/main/java/io/kpereira/permissionschallenge/ui/requestPermissions/RequestPermissionFragment.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,61 +18,52 @@ import java.util.List;
 import io.kpereira.permissionschallenge.R;
 import io.kpereira.permissionschallenge.adapter.RequestPermissionRecyclerViewAdapter;
 import io.kpereira.permissionschallenge.model.Permission;
+import io.kpereira.permissionschallenge.model.PermissionViewModel;
 
 public class RequestPermissionFragment extends Fragment {
+    List<Permission> permissionList = new ArrayList<>();
+    private PermissionViewModel permissionViewModel;
     private RecyclerView recyclerView;
     private RequestPermissionRecyclerViewAdapter recyclerViewAdapter;
-    List<Permission> permissions = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_request_permissions, container, false);
+        View root = inflater.inflate(R.layout.fragment_request_permissions,
+                container, false);
+        permissionViewModel = new ViewModelProvider(this.requireActivity())
+                .get(PermissionViewModel.class);
 
         recyclerView = root.findViewById(R.id.set_permissions_recycleview);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        this.permissions.add(new Permission( "Write Contacts",
+        this.permissionList.add(new Permission("Write Contacts",
                 android.Manifest.permission.WRITE_CONTACTS));
-        this.permissions.add(new Permission("Write External Storage",
+        this.permissionList.add(new Permission("Write External Storage",
                 android.Manifest.permission.WRITE_EXTERNAL_STORAGE));
-        this.permissions.add(new Permission("Read SMS",
+        this.permissionList.add(new Permission("Read SMS",
                 android.Manifest.permission.READ_SMS));
-        this.permissions.add(new Permission("Camera",
+        this.permissionList.add(new Permission("Camera",
                 android.Manifest.permission.CAMERA));
-        this.permissions.add(new Permission("ACCEPT_HANDOVER",
-                android.Manifest.permission.ACCEPT_HANDOVER));
-        this.permissions.add(new Permission("Background Locarion",
-                android.Manifest.permission.ACCESS_BACKGROUND_LOCATION));
-        this.permissions.add(new Permission("Access Network State",
-                android.Manifest.permission.ACCESS_NETWORK_STATE));
-        this.permissions.add(new Permission("Access Wifi State",
-                android.Manifest.permission.ACCESS_WIFI_STATE));
-        this.permissions.add(new Permission("Answer Phone Calls",
+        this.permissionList.add(new Permission("Answer Phone Calls",
                 android.Manifest.permission.ANSWER_PHONE_CALLS));
-        this.permissions.add(new Permission("Bluetooth",
-                android.Manifest.permission.BLUETOOTH));
-        this.permissions.add(new Permission("Body Sensors",
+        this.permissionList.add(new Permission("Body Sensors",
                 android.Manifest.permission.BODY_SENSORS));
-        this.permissions.add(new Permission("Broadcast Sticky",
-                android.Manifest.permission.BROADCAST_STICKY));
-        this.permissions.add(new Permission("Expand Status Bar",
-                android.Manifest.permission.EXPAND_STATUS_BAR));
-        this.permissions.add(new Permission("Get Accounts",
-                android.Manifest.permission.GET_ACCOUNTS));
-        this.permissions.add(new Permission("Get Package Size",
-                android.Manifest.permission.GET_PACKAGE_SIZE));
-        this.permissions.add(new Permission("Install Shorcut",
-                android.Manifest.permission.INTERNET));
-        this.permissions.add(new Permission("NFC",
-                android.Manifest.permission.NFC));
-        this.permissions.add(new Permission("Write Calendar",
+        this.permissionList.add(new Permission("Write Calendar",
                 android.Manifest.permission.WRITE_CALENDAR));
-        this.permissions.add(new Permission("Record Audio",
+        this.permissionList.add(new Permission("Record Audio",
                 android.Manifest.permission.RECORD_AUDIO));
 
-        recyclerViewAdapter = new RequestPermissionRecyclerViewAdapter(getContext(), permissions);
-        recyclerView.setAdapter(recyclerViewAdapter);
+        for (Permission permission : permissionList)
+            permissionViewModel.insert(permission);
+
+        permissionViewModel.getAllPermissions().observe(getViewLifecycleOwner(), new Observer<List<Permission>>() {
+            @Override
+            public void onChanged(List<Permission> permissions) {
+                recyclerViewAdapter = new RequestPermissionRecyclerViewAdapter(getContext(), permissions);
+                recyclerView.setAdapter(recyclerViewAdapter);
+            }
+        });
         return root;
     }
 }
--- app/src/main/java/io/kpereira/permissionschallenge/ui/showPermissions/ShowPermissionsFragment.java
+++ app/src/main/java/io/kpereira/permissionschallenge/ui/showPermissions/ShowPermissionsFragment.java
@@ -7,70 +7,41 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import io.kpereira.permissionschallenge.R;
 import io.kpereira.permissionschallenge.adapter.ShowPermissionRecyclerViewAdapter;
 import io.kpereira.permissionschallenge.model.Permission;
+import io.kpereira.permissionschallenge.model.PermissionViewModel;
 
 public class ShowPermissionsFragment extends Fragment {
+    private PermissionViewModel permissionViewModel;
     private RecyclerView recyclerView;
     private ShowPermissionRecyclerViewAdapter showPermissionRecyclerViewAdapter;
-    private List<Permission> permissionList = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_show_permissions, container, false);
+        View root = inflater.inflate(R.layout.fragment_show_permissions,
+                container, false);
+        permissionViewModel = new ViewModelProvider(this.requireActivity())
+                .get(PermissionViewModel.class);
 
         recyclerView = root.findViewById(R.id.list_permissions_recycleview);
         recyclerView.hasFixedSize();
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-        this.permissionList.add(new Permission( "Write Contacts",
-                android.Manifest.permission.WRITE_CONTACTS));
-        this.permissionList.add(new Permission("Write External Storage",
-                android.Manifest.permission.WRITE_EXTERNAL_STORAGE));
-        this.permissionList.add(new Permission("Read SMS",
-                android.Manifest.permission.READ_SMS));
-        this.permissionList.add(new Permission("Camera",
-                android.Manifest.permission.CAMERA));
-        this.permissionList.add(new Permission("ACCEPT_HANDOVER",
-                android.Manifest.permission.ACCEPT_HANDOVER));
-        this.permissionList.add(new Permission("Background Locarion",
-                android.Manifest.permission.ACCESS_BACKGROUND_LOCATION));
-        this.permissionList.add(new Permission("Access Network State",
-                android.Manifest.permission.ACCESS_NETWORK_STATE));
-        this.permissionList.add(new Permission("Access Wifi State",
-                android.Manifest.permission.ACCESS_WIFI_STATE));
-        this.permissionList.add(new Permission("Answer Phone Calls",
-                android.Manifest.permission.ANSWER_PHONE_CALLS));
-        this.permissionList.add(new Permission("Bluetooth",
-                android.Manifest.permission.BLUETOOTH));
-        this.permissionList.add(new Permission("Body Sensors",
-                android.Manifest.permission.BODY_SENSORS));
-        this.permissionList.add(new Permission("Broadcast Sticky",
-                android.Manifest.permission.BROADCAST_STICKY));
-        this.permissionList.add(new Permission("Expand Status Bar",
-                android.Manifest.permission.EXPAND_STATUS_BAR));
-        this.permissionList.add(new Permission("Get Accounts",
-                android.Manifest.permission.GET_ACCOUNTS));
-        this.permissionList.add(new Permission("Get Package Size",
-                android.Manifest.permission.GET_PACKAGE_SIZE));
-        this.permissionList.add(new Permission("Install Shorcut",
-                android.Manifest.permission.INTERNET));
-        this.permissionList.add(new Permission("NFC",
-                android.Manifest.permission.NFC));
-        this.permissionList.add(new Permission("Write Calendar",
-                android.Manifest.permission.WRITE_CALENDAR));
-        this.permissionList.add(new Permission("Record Audio",
-                android.Manifest.permission.RECORD_AUDIO));
-
-        showPermissionRecyclerViewAdapter = new ShowPermissionRecyclerViewAdapter(getContext(), permissionList);
-        recyclerView.setAdapter(showPermissionRecyclerViewAdapter);
+        permissionViewModel.getAllPermissions().observe(getViewLifecycleOwner(), new Observer<List<Permission>>() {
+            @Override
+            public void onChanged(List<Permission> permissions) {
+                showPermissionRecyclerViewAdapter = new ShowPermissionRecyclerViewAdapter(getContext(), permissions);
+                recyclerView.setAdapter(showPermissionRecyclerViewAdapter);
+            }
+        });
         return root;
     }
 }
